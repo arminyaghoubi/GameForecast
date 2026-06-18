f07400cc0979cbcf978262788b36bbdd2dde3fd4
Armin Yaghoubi <arminyaghoubi1@gmail.com>
2026-06-18 19:06:29 +0330

    add command and query abstractions with scrutor scanning #7

---
+++ src/BuildingBlocks/BuildingBlocksRegistration.cs
@@ -0,0 +1,20 @@
+﻿using BuildingBlocks.Messaging;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace BuildingBlocks;
+
+public static class BuildingBlocksRegistration
+{
+    public static IServiceCollection AddMeditor(this IServiceCollection services, params Assembly[] assemblies)
+    {
+        services.AddScoped<Mediator>();
+        services.Scan(scan => scan
+            .FromAssemblies(assemblies)
+            .AddClasses(c => c.AssignableTo(typeof(IHandler<,>)))
+            .AsImplementedInterfaces()
+            .WithScopedLifetime());
+
+        return services;
+    }
+}
